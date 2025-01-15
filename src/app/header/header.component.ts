4a9500947c83a3cbfb49a677d0f8748c4f807f74
22051988Di <vitalii.soloninko@gmail.com>
2025-01-15 10:18:42 +0100

    0.3	Header

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,28 @@
+import { NgFor } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  imports: [RouterLink, NgFor],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {
+  openMenu = false;
+
+  menuItems = [
+    {
+      name: 'Spreiseplan',
+      link: 'menu',
+    },
+    {
+      name: 'Warenkorb',
+      link: 'cart',
+    },
+    {
+      name: 'History',
+      link: 'history',
+    },
+  ];
+}
