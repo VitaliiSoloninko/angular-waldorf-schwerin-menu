cf0312d9458c2a42a11cead94609fef477934403
22051988Di <vitalii.soloninko@gmail.com>
2025-01-16 10:45:51 +0100

    0.4	Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './pages/auth/auth.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { HistoryComponent } from './pages/history/history.component';
+import { MenuComponent } from './pages/menu/menu.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MenuComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'history', component: HistoryComponent },
+  { path: 'auth', component: AuthComponent },
+];
