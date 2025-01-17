bcce5021fe33a15a9b61c68c65c6c9547e10e9d9
22051988Di <vitalii.soloninko@gmail.com>
2025-01-17 11:38:58 +0100

    0.6	Footer + Lucide icons

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import {
+  Calendar,
+  LucideAngularModule,
+  ShoppingCart,
+  User,
+  Utensils,
+} from 'lucide-angular';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [LucideAngularModule, RouterLink],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
 export class FooterComponent {
-
+  utensils: any = Utensils;
+  shoppingCart: any = ShoppingCart;
+  calendar = Calendar;
+  user: any = User;
 }
