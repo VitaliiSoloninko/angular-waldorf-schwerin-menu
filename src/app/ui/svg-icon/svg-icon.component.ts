e5d64524ce465fa871bfea33636381950175284f
22051988Di <vitalii.soloninko@gmail.com>
2025-03-20 11:47:27 +0100

    4.7.1	Svg icon description

--- src/app/ui/svg-icon/svg-icon.component.ts
+++ src/app/ui/svg-icon/svg-icon.component.ts
@@ -13,3 +13,23 @@ export class SvgIconComponent {
     return `/assets/svg/${this.icon}.svg#${this.icon}`;
   }
 }
+
+// HTML
+// <div class="svg-block">
+//     <svg icon="cart"></svg>
+//   </div>
+
+// CSS
+// .svg-block {
+//   width: 24px;
+//   height: 24px;
+//   color: grey;
+//   transition: color 0.3s ease 0s;
+// }
+
+// .svg-block {
+//   &:hover,
+//   &:focus-visible {
+//     color: green;
+//   }
+// }
