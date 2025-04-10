0b720d4222f97a312b908b7d2fc8ea475315f5c6
22051988Di <vitalii.soloninko@gmail.com>
2025-04-10 13:09:30 +0200

    6.9.1	Angular json maximumWarning 2MB

---
+++ src/app/pages/admin-page/user-orders-month/user-orders-month.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-orders-month',
+  imports: [],
+  templateUrl: './user-orders-month.component.html',
+  styleUrl: './user-orders-month.component.scss'
+})
+export class UserOrdersMonthComponent {
+
+}
