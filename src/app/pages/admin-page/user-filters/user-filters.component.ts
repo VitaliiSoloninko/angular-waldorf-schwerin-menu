07379aec090e01d4635c094d687451d8cf4d96f2
22051988Di <vitalii.soloninko@gmail.com>
2025-04-22 07:39:27 +0200

    6.23.1	Admin. Users. Table add id and №

---
+++ src/app/pages/admin-page/user-filters/user-filters.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-filters',
+  imports: [],
+  templateUrl: './user-filters.component.html',
+  styleUrl: './user-filters.component.scss'
+})
+export class UserFiltersComponent {
+
+}
