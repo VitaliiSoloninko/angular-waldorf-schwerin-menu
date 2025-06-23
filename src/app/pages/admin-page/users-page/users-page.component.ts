f7367979374f3263e51697c8ef1d7e3adb5c86ec
22051988Di <vitalii.soloninko@gmail.com>
2025-06-23 17:50:51 +0200

    Admin. Users. Add search (part 1)

--- src/app/pages/admin-page/users-page/users-page.component.ts
+++ src/app/pages/admin-page/users-page/users-page.component.ts
@@ -1,16 +1,16 @@
-import { NgFor } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { Router, RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 import { FileText, LucideAngularModule, Pencil, Trash2 } from 'lucide-angular';
 
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { DateTime } from 'luxon';
 import { User } from '../../../models/user.model';
 import { UserService } from '../../../services/user.service';
-import { UserFiltersComponent } from './user-filters/user-filters.component';
 
 @Component({
   selector: 'app-users-page',
-  imports: [NgFor, RouterLink, LucideAngularModule, UserFiltersComponent],
+  imports: [LucideAngularModule, CommonModule, FormsModule],
   templateUrl: './users-page.component.html',
   styleUrl: './users-page.component.scss',
 })
@@ -18,13 +18,15 @@ export class UsersPageComponent implements OnInit {
   fileText: any = FileText;
   trash2: any = Trash2;
   pencil: any = Pencil;
-
   currentMonth: number = DateTime.now().month;
   currentYear: number = DateTime.now().year;
+  users: User[] = [];
+
+  searchId: number | null = null;
+  foundUser: User | null = null;
+  error: string | null = null;
 
   constructor(private userService: UserService, private router: Router) {}
-
-  users: User[] = [];
 
   ngOnInit(): void {
     this.userService.getAll().subscribe((data) => {
@@ -46,5 +48,23 @@ export class UsersPageComponent implements OnInit {
 
   goToUserMonthOrders(userId: number): void {
     this.router.navigate([`/admin/user/${userId}`]);
+  }
+
+  searchUserById() {
+    if (!this.searchId) {
+      this.foundUser = null;
+      this.error = null;
+      return;
+    }
+    this.userService.getUserById(this.searchId).subscribe({
+      next: (user) => {
+        this.foundUser = user;
+        this.error = null;
+      },
+      error: () => {
+        this.foundUser = null;
+        this.error = 'Benutzer nicht gefunden.';
+      },
+    });
   }
 }
