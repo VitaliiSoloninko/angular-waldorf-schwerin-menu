9bede29f5e90c7191903272f36f1be3a4995f297
22051988Di <vitalii.soloninko@gmail.com>
2025-07-06 22:35:07 +0200

    Registration. Dynamic form

--- src/app/pages/registration-page/registration-page.component.ts
+++ src/app/pages/registration-page/registration-page.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { Component, Input, OnInit, signal } from '@angular/core';
 import {
   AbstractControl,
   AsyncValidatorFn,
@@ -8,8 +8,8 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
-import { Router, RouterLink } from '@angular/router';
-import { debounceTime, first, map, of, switchMap, tap } from 'rxjs';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { debounceTime, first, map, of, switchMap } from 'rxjs';
 import { UserService } from '../../services/user.service';
 import { SvgIconComponent } from '../../ui/svg-icon/svg-icon.component';
 
@@ -19,10 +19,16 @@ import { SvgIconComponent } from '../../ui/svg-icon/svg-icon.component';
   templateUrl: './registration-page.component.html',
   styleUrl: './registration-page.component.scss',
 })
-export class RegistrationPageComponent {
-  constructor(private router: Router, private userService: UserService) {}
+export class RegistrationPageComponent implements OnInit {
+  form!: FormGroup;
+  constructor(
+    private router: Router,
+    private userService: UserService,
+    private route: ActivatedRoute
+  ) {}
   isPasswordVisible = signal<boolean>(false);
   emailExists = false;
+  @Input() isTeacher = false;
 
   emailAsyncValidator: AsyncValidatorFn = (control: AbstractControl) => {
     return of(control.value).pipe(
@@ -31,7 +37,6 @@ export class RegistrationPageComponent {
         email
           ? this.userService.checkEmailExists(email).pipe(
               map((res) => (res.exists ? { emailExists: true } : null)),
-              tap((result) => console.log('Async validator result:', result)),
               first()
             )
           : of(null)
@@ -39,46 +44,59 @@ export class RegistrationPageComponent {
     );
   };
 
-  form = new FormGroup({
-    email: new FormControl('', {
-      validators: [Validators.email, Validators.required],
-      asyncValidators: [this.emailAsyncValidator],
-      updateOn: 'change',
-    }),
-    password: new FormControl('', {
-      validators: [Validators.required, Validators.minLength(6)],
-    }),
-    firstName: new FormControl('', {
-      validators: [Validators.required],
-    }),
-    lastName: new FormControl('', {
-      validators: [Validators.required],
-    }),
-    firstNameChild: new FormControl('', {
-      validators: [Validators.required],
-    }),
-    lastNameChild: new FormControl('', {
-      validators: [Validators.required],
-    }),
-    street: new FormControl('', {
-      validators: [Validators.required],
-    }),
-    number: new FormControl('', {
-      validators: [Validators.required],
-    }),
-    postalCode: new FormControl('', {
-      validators: [Validators.required],
-    }),
-    city: new FormControl('', {
-      validators: [Validators.required],
-    }),
-    school: new FormControl<'Waldorf'>('Waldorf', {
-      validators: [Validators.required],
-    }),
-    class: new FormControl<'1' | '2' | '3' | '4'>('1', {
-      validators: [Validators.required],
-    }),
-  });
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      this.isTeacher = params['teacher'] === 'true';
+      this.createForm();
+    });
+  }
+
+  createForm() {
+    this.form = new FormGroup({
+      email: new FormControl('', {
+        validators: [Validators.email, Validators.required],
+        asyncValidators: [this.emailAsyncValidator],
+        updateOn: 'change',
+      }),
+      password: new FormControl('', {
+        validators: [Validators.required, Validators.minLength(6)],
+      }),
+      firstName: new FormControl('', {
+        validators: [Validators.required],
+      }),
+      lastName: new FormControl('', {
+        validators: [Validators.required],
+      }),
+      street: new FormControl('', {
+        validators: [Validators.required],
+      }),
+      number: new FormControl('', {
+        validators: [Validators.required],
+      }),
+      postalCode: new FormControl('', {
+        validators: [Validators.required],
+      }),
+      city: new FormControl('', {
+        validators: [Validators.required],
+      }),
+      school: new FormControl<'Waldorf'>('Waldorf', {
+        validators: [Validators.required],
+      }),
+      ...(this.isTeacher
+        ? {}
+        : {
+            firstNameChild: new FormControl('', {
+              validators: [Validators.required],
+            }),
+            lastNameChild: new FormControl('', {
+              validators: [Validators.required],
+            }),
+            class: new FormControl<'1' | '2' | '3' | '4'>('1', {
+              validators: [Validators.required],
+            }),
+          }),
+    });
+  }
 
   get emailIsInvalid() {
     const email = this.form.controls.email;
@@ -91,7 +109,6 @@ export class RegistrationPageComponent {
     if (this.form.invalid) {
       return;
     }
-    //@ts-ignore
     this.userService.createUser(this.form.value).subscribe({
       next: () => {
         this.router.navigate(['/login']);
