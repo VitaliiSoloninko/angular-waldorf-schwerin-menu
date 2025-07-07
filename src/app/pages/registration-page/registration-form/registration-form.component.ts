21fa5d2cde03fb2819f4f7b6b3993ad0dce7f10e
22051988Di <vitalii.soloninko@gmail.com>
2025-07-07 19:32:40 +0200

    Registration. Separation of form and page

---
+++ src/app/pages/registration-page/registration-form/registration-form.component.ts
@@ -0,0 +1,127 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit, signal } from '@angular/core';
+import {
+  AbstractControl,
+  AsyncValidatorFn,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { debounceTime, first, map, of, switchMap } from 'rxjs';
+import { UserService } from '../../../services/user.service';
+import { SvgIconComponent } from '../../../ui/svg-icon/svg-icon.component';
+
+@Component({
+  selector: 'app-registration-form',
+  imports: [ReactiveFormsModule, SvgIconComponent, CommonModule],
+  templateUrl: './registration-form.component.html',
+  styleUrl: './registration-form.component.scss',
+})
+export class RegistrationFormComponent implements OnInit {
+  form!: FormGroup;
+  constructor(
+    private router: Router,
+    private userService: UserService,
+    private route: ActivatedRoute
+  ) {}
+  isPasswordVisible = signal<boolean>(false);
+  emailExists = false;
+  @Input() isTeacher = false;
+
+  emailAsyncValidator: AsyncValidatorFn = (control: AbstractControl) => {
+    return of(control.value).pipe(
+      debounceTime(500),
+      switchMap((email) =>
+        email
+          ? this.userService.checkEmailExists(email).pipe(
+              map((res) => (res.exists ? { emailExists: true } : null)),
+              first()
+            )
+          : of(null)
+      )
+    );
+  };
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      this.isTeacher = params['teacher'] === 'true';
+      this.createForm();
+    });
+  }
+
+  createForm() {
+    this.form = new FormGroup({
+      email: new FormControl('', {
+        validators: [Validators.email, Validators.required],
+        asyncValidators: [this.emailAsyncValidator],
+        updateOn: 'change',
+      }),
+      password: new FormControl('', {
+        validators: [Validators.required, Validators.minLength(6)],
+      }),
+      firstName: new FormControl('', {
+        validators: [Validators.required],
+      }),
+      lastName: new FormControl('', {
+        validators: [Validators.required],
+      }),
+      street: new FormControl('', {
+        validators: [Validators.required],
+      }),
+      number: new FormControl('', {
+        validators: [Validators.required],
+      }),
+      postalCode: new FormControl('', {
+        validators: [Validators.required],
+      }),
+      city: new FormControl('', {
+        validators: [Validators.required],
+      }),
+      school: new FormControl<'Waldorf'>('Waldorf', {
+        validators: [Validators.required],
+      }),
+      ...(this.isTeacher
+        ? {}
+        : {
+            firstNameChild: new FormControl('', {
+              validators: [Validators.required],
+            }),
+            lastNameChild: new FormControl('', {
+              validators: [Validators.required],
+            }),
+            class: new FormControl<'1' | '2' | '3' | '4'>('1', {
+              validators: [Validators.required],
+            }),
+          }),
+    });
+  }
+
+  get emailIsInvalid() {
+    const email = this.form.controls.email;
+    return email.touched && email.dirty && email.invalid && email.value;
+  }
+
+  createNewUser() {
+    this.emailExists = false;
+
+    if (this.form.invalid) {
+      return;
+    }
+    this.userService.createUser(this.form.value).subscribe({
+      next: () => {
+        this.router.navigate(['/login']);
+      },
+      error: (err) => {
+        if (err.status === 400 && err.error?.message?.includes('email')) {
+          this.emailExists = true;
+        }
+      },
+    });
+  }
+
+  onReset() {
+    this.form.reset();
+  }
+}
--- src/app/pages/registration-page/registration-page.component.ts
+++ src/app/pages/registration-page/registration-page.component.ts
@@ -1,127 +1,30 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit, signal } from '@angular/core';
-import {
-  AbstractControl,
-  AsyncValidatorFn,
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
-import { ActivatedRoute, Router, RouterLink } from '@angular/router';
-import { debounceTime, first, map, of, switchMap } from 'rxjs';
-import { UserService } from '../../services/user.service';
-import { SvgIconComponent } from '../../ui/svg-icon/svg-icon.component';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
 
 @Component({
   selector: 'app-registration-page',
-  imports: [ReactiveFormsModule, RouterLink, SvgIconComponent, CommonModule],
+  imports: [
+    ReactiveFormsModule,
+    RouterLink,
+    CommonModule,
+    RegistrationFormComponent,
+  ],
   templateUrl: './registration-page.component.html',
   styleUrl: './registration-page.component.scss',
 })
 export class RegistrationPageComponent implements OnInit {
   form!: FormGroup;
-  constructor(
-    private router: Router,
-    private userService: UserService,
-    private route: ActivatedRoute
-  ) {}
+  constructor(private route: ActivatedRoute) {}
   isPasswordVisible = signal<boolean>(false);
   emailExists = false;
   @Input() isTeacher = false;
 
-  emailAsyncValidator: AsyncValidatorFn = (control: AbstractControl) => {
-    return of(control.value).pipe(
-      debounceTime(500),
-      switchMap((email) =>
-        email
-          ? this.userService.checkEmailExists(email).pipe(
-              map((res) => (res.exists ? { emailExists: true } : null)),
-              first()
-            )
-          : of(null)
-      )
-    );
-  };
-
   ngOnInit(): void {
     this.route.queryParams.subscribe((params) => {
       this.isTeacher = params['teacher'] === 'true';
-      this.createForm();
     });
-  }
-
-  createForm() {
-    this.form = new FormGroup({
-      email: new FormControl('', {
-        validators: [Validators.email, Validators.required],
-        asyncValidators: [this.emailAsyncValidator],
-        updateOn: 'change',
-      }),
-      password: new FormControl('', {
-        validators: [Validators.required, Validators.minLength(6)],
-      }),
-      firstName: new FormControl('', {
-        validators: [Validators.required],
-      }),
-      lastName: new FormControl('', {
-        validators: [Validators.required],
-      }),
-      street: new FormControl('', {
-        validators: [Validators.required],
-      }),
-      number: new FormControl('', {
-        validators: [Validators.required],
-      }),
-      postalCode: new FormControl('', {
-        validators: [Validators.required],
-      }),
-      city: new FormControl('', {
-        validators: [Validators.required],
-      }),
-      school: new FormControl<'Waldorf'>('Waldorf', {
-        validators: [Validators.required],
-      }),
-      ...(this.isTeacher
-        ? {}
-        : {
-            firstNameChild: new FormControl('', {
-              validators: [Validators.required],
-            }),
-            lastNameChild: new FormControl('', {
-              validators: [Validators.required],
-            }),
-            class: new FormControl<'1' | '2' | '3' | '4'>('1', {
-              validators: [Validators.required],
-            }),
-          }),
-    });
-  }
-
-  get emailIsInvalid() {
-    const email = this.form.controls.email;
-    return email.touched && email.dirty && email.invalid && email.value;
-  }
-
-  createNewUser() {
-    this.emailExists = false;
-
-    if (this.form.invalid) {
-      return;
-    }
-    this.userService.createUser(this.form.value).subscribe({
-      next: () => {
-        this.router.navigate(['/login']);
-      },
-      error: (err) => {
-        if (err.status === 400 && err.error?.message?.includes('email')) {
-          this.emailExists = true;
-        }
-      },
-    });
-  }
-
-  onReset() {
-    this.form.reset();
   }
 }
